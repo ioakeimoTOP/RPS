302c09d46b3797c496b94bc25e749d7f1ed2fa6b
ioakeimo <ioakeimo.dev@gmail.com>
2023-04-01 23:39:35 +0300

    Add utility function for building elements
    Creating complete element nodes on the fly with higher flexibility is needed
    to keep the code as clean as possible.

--- src/modules/utils.mjs
+++ src/modules/utils.mjs
@@ -1,3 +1,14 @@
 export function getRandomIndex(size) {
   return Math.floor(Math.random() * size);
 }
+
+export function elementBuilder(tag, classes = [], ...attributes) {
+  const element = document.createElement(tag);
+  for (const cls of classes) {
+    element.classList.add(cls);
+  }
+  for (const [attribute, value] of attributes) {
+    element.setAttribute(attribute, value);
+  }
+  return element;
+}
